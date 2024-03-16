c0ccd4f4f4e05d0cd8498b19d81436defd66466e
miki553 <65011212031@msu.ac.th>
2024-03-16 22:33:11 +0700

    first commit

--- app.ts
+++ app.ts
@@ -1,8 +1,12 @@
 import express from "express";
-// const express = require("express");
+import{router as upload} from "./upload";
+
 
 
 export const app = express();
+
+app.use("/upload",upload)
+
 app.use("/", (req, res) => {
   res.send("Hello World!!!");
 });
---
+++ upload.ts
@@ -0,0 +1,91 @@
+import express from "express";
+import path from "path";
+import multer from "multer";
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL,deleteObject } from "firebase/storage";
+
+export const router = express.Router();
+
+
+router.get("/",(req,res)=>{
+    res.send("Upload");
+});
+
+
+
+
+// key ก่อน
+// apiKey: "AIzaSyC-NHZoh1twNsTs0PHjkCIydxcHISrCqEE"
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDB0wentj8Igqan_Bpe7g-MOEvCSSiSnt0",
+    authDomain: "api-test-94f3b.firebaseapp.com",
+    projectId: "api-test-94f3b",
+    storageBucket: "api-test-94f3b.appspot.com",
+    messagingSenderId: "291742907529",
+    appId: "1:291742907529:web:ea2f1dc03bcbde0462da58",
+    measurementId: "G-P99T31SED3"
+};
+
+
+initializeApp(firebaseConfig);
+const storage = getStorage();
+
+
+class FileMiddleware {
+    filename = "";
+    public readonly diskLoader = multer({
+        storage: multer.memoryStorage(),
+        limits: {
+            fileSize: 67108864 
+        }
+    });
+}
+const fileupload = new FileMiddleware();
+
+
+
+
+//add
+router.post("/", fileupload.diskLoader.single("file"), async (req, res) => {
+    if (req.file && req.file.mimetype) {
+        const filename = Math.round(Math.random() * 100000) + '.png';
+        const storageRef = ref(storage, "/images/" + filename);
+        const metadata = { contentType: req.file.mimetype };
+        const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
+        const url = await getDownloadURL(snapshot.ref);
+        res.status(200).json({
+            filename: filename,
+            url: url
+        });
+    } else {
+        res.status(400).json({ error: 'No file uploaded or invalid file' });
+    }
+});
+
+
+
+//delete
+router.delete("/:name", async (req, res) => {
+  const filenameToDelete =req.params.name;
+  const fileRef = ref(storage, "/images/"+ filenameToDelete);
+  try {
+    await deleteObject(fileRef);
+    res.status(200).json({ message: `File ${filenameToDelete} has been deleted successfully.` });
+  } catch (error) {
+    console.error(`Error deleting file ${filenameToDelete}:`, error);
+    res.status(500).json({ error: 'An error occurred while deleting the file.' });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
